dad3fdf9af20983b67a89f14d2db050c4d0161fb
Kristin Smith <krysia.codes@gmail.com>
2016-03-24 18:32:26 -0400

    Adds Specialties from Info Kiosk

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324222138) do
+ActiveRecord::Schema.define(version: 20160324223007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 20160324222138) do
   create_table "recruiters", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.integer  "bullhorn_id"
+    t.integer  "category_id"
   end
 
   create_table "timeslots", force: :cascade do |t|
