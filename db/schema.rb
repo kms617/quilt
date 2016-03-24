e13d0d3226619439ac2bda91bb06cf5319ffe4f8
Kristin Smith <krysia.codes@gmail.com>
2016-03-24 18:34:52 -0400

    Adds Specializations from Info Kiosk

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324223007) do
+ActiveRecord::Schema.define(version: 20160324223256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,16 @@ ActiveRecord::Schema.define(version: 20160324223007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "specializations", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "specialty_id"
+    t.integer  "candidate_id"
+  end
+
+  add_index "specializations", ["candidate_id"], name: "index_specializations_on_candidate_id", using: :btree
+  add_index "specializations", ["specialty_id"], name: "index_specializations_on_specialty_id", using: :btree
 
   create_table "specialties", force: :cascade do |t|
     t.datetime "created_at"
