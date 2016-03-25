7e7c59dab048d534b55434e097a2f729613cecd0
Kristin Smith <krysia.codes@gmail.com>
2016-03-24 23:38:23 -0400

    Adds Adecco colors and some styling to new candidate form

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Quilt
     end
 
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join('assets', 'fonts') 
     config.assets.initialize_on_precompile = false
   end
 end
