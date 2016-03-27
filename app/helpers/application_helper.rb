9ddd2204c6d51aa421a1fbb1f1c30b4733bf66e1
Kristin Smith <krysia.codes@gmail.com>
2016-03-27 19:17:36 -0400

    Adds EEOC modal with nil options for opt out to form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def genders_for_select
-    ["Male", "Female", "Other"]
+    [nil, "Male", "Female", "Other"]
   end
 
   def races_for_select
-    ["Hispanic or Latino", "White (Not Hispanic or Latino)", "Black or African American (Not Hispanic or Latino)", "Native Hawaiian or Other Pacific Islander (Not Hispanic or Latino)", "Asian (Not Hispanic or Latino)", "American Indian or Alaska Native (Not Hispanic or Latino)", "Two or More Races (Not Hispanic or Latino)"]
+    [nil, "Hispanic or Latino", "White (Not Hispanic or Latino)", "Black or African American (Not Hispanic or Latino)", "Native Hawaiian or Other Pacific Islander (Not Hispanic or Latino)", "Asian (Not Hispanic or Latino)", "American Indian or Alaska Native (Not Hispanic or Latino)", "Two or More Races (Not Hispanic or Latino)"]
   end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,6 +1,4 @@
 class Candidate < ActiveRecord::Base
   has_one :appointment
   belongs_to :category
-
-  accepts_nested_attributes_for :categories
 end
