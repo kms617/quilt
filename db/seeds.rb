986a3d57e45735ffd599c7d6bc4f0ac1165a1678
Leon <leonurusso@gmail.com>
2016-03-31 13:34:35 -0400

    Edited Seed File to Correct Emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,11 @@ end
 
 # RECRUITERS
 Recruiter.create(id: 11, first_name: 'Son', last_name: "Trinh", email: "son@bullhorn.com", password: "password", password_confirmation: "password")
-Recruiter.create(id: 12, first_name: 'Kristen', last_name: "Smith", email: "kristen@bullhorn.com", password: "password", password_confirmation: "password")
-Recruiter.create(id: 13, first_name: 'Leon', last_name: "Russo", email: "leon@bullhorn.com", password: "password", password_confirmation: "password")
-Recruiter.create(id: 14, first_name: 'Melanie', last_name: "Mosquera", email: "melanie@bullhorn.com", password: "password", password_confirmation: "password")
-Recruiter.create(id: 15, first_name: 'Chris', last_name: "Vasich", email: "chris@bullhorn.com", password: "password", password_confirmation: "password")
-Recruiter.create(id: 16, first_name: 'Dennis', last_name: "McQuilken", email: "dennis@bullhorn.com", password: "password", password_confirmation: "password")
+Recruiter.create(id: 12, first_name: 'Kristin', last_name: "Smith", email: "k.smith@bullhorn.com", password: "password", password_confirmation: "password")
+Recruiter.create(id: 13, first_name: 'Leon', last_name: "Russo", email: "leon.russo@bullhorn.com", password: "password", password_confirmation: "password")
+Recruiter.create(id: 14, first_name: 'Melanie', last_name: "Mosquera", email: "mmosquera@bullhorn.com", password: "password", password_confirmation: "password")
+Recruiter.create(id: 15, first_name: 'Chris', last_name: "Vasich", email: "cvasich@bullhorn.com", password: "password", password_confirmation: "password")
+Recruiter.create(id: 16, first_name: 'Dennis', last_name: "McQuilken", email: "dmcquilken@bullhorn.com", password: "password", password_confirmation: "password")
 
 # APPOINTMENTS
 Timeslot.all.each do |t|
