3a9198c6302e504cb46fcd9a9cc742ef00c8cb5b
Kristin Smith <krysia.codes@gmail.com>
2016-03-31 01:21:31 -0400

    Adds bullhorn_id to Categories seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Category.create(name: 'Clerical', image: 'clerical.png')
-Category.create(name: 'Finance', image: 'finance.png')
-Category.create(name: 'Manufacturing', image: 'manufacturing.png')
-Category.create(name: 'Hospitality', image: 'hospitality.png')
+Category.create(name: 'Clerical', image: 'clerical.png', bullhorn_id: 1472778)
+Category.create(name: 'Accounting and Finance', image: 'finance.png', bullhorn_id: 1472765)
+Category.create(name: 'Manufacturing', image: 'manufacturing.png', bullhorn_id: 1472805)
+Category.create(name: 'Hospitality', image: 'hospitality.png', bullhorn_id: 1472801)
