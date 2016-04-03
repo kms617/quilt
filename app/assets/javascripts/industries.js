d20289cf006f658b326dd51afc90b513de540be7
Kristin Smith <krysia.codes@gmail.com>
2016-04-03 18:37:10 -0400

    Minor style adjustments to appointments index, topbar bar candidates

--- app/assets/javascripts/industries.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-
-    $("#li").animate({left: "0"}, {
-    duration: 2000
-    });
-});
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -18,8 +18,8 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
     @candidate = Candidate.find(params[:candidate_id])
     @appointment.candidate = @candidate
-    flash[:notice] = "#{@appointment.recruiter.first_name} will be with you shortly. Thank you."
-    redirect_to welcome_home_path
+      flash[:notice] = "#{@appointment.recruiter.first_name} will be ready for you at #{@appointment.timeslot.start_time.strftime('%l:%M %P')}. Thank you for signing up!"
+      redirect_to welcome_home_path
   end
 
   def new
